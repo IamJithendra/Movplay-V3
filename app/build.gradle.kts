a7d28a8764d9e7414e8f848cbc095cc8f569db49
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-20 00:22:57 +0530

    moved all dependencies to catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ android {
 }
 
 dependencies {
+
+    // Compose
     implementation(libs.androidx.core.core.ktx)
     implementation(libs.androidx.compose.ui)
 //    implementation("androidx.compose.ui:ui:1.3.0-rc01")
@@ -87,58 +89,50 @@ dependencies {
     implementation(libs.com.airbnb.android.lottie.compose)
     implementation(libs.androidx.lifecycle.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.activity.compose)
-    implementation("androidx.test:runner:1.4.0")
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.test.espresso.espresso.core)
-    androidTestImplementation(libs.androidx.compose.ui.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.ui.test.manifest)
+    implementation(libs.androidx.runner)
 
-    //Foundation
+    // Foundation
     implementation(libs.androidx.compose.foundation)
 
-    //Paging-Compose
+    // Paging-Compose
     implementation(libs.androidx.paging.paging.compose)
 
-    //icon
+    // Icon
     implementation(libs.androidx.compose.material.material.icons.extended)
 
-    //Dagger-hilt
+    // Dagger-hilt
     implementation(libs.com.google.dagger.hilt.android)
     kapt(libs.com.google.dagger.hilt.android.compiler)
     implementation(libs.androidx.hilt.hilt.navigation.compose)
     kapt(libs.androidx.hilt.hilt.compiler)
 
-    //Unit-test
-
-    //Moshi
+    // Moshi
     implementation(libs.com.squareup.moshi)
-    kapt(libs.com.squareup.moshi.moshi.kotlin.codegen)
+    ksp(libs.com.squareup.moshi.moshi.kotlin.codegen)
 
-    //Retrofit
+    // Retrofit
     implementation(libs.com.squareup.retrofit2.retrofit)
     implementation(libs.com.squareup.retrofit2.converter.moshi)
 
-    //OkHttp
+    // OkHttp
     implementation(libs.com.squareup.okhttp3.okhttp)
     implementation(libs.com.squareup.okhttp3.logging.interceptor)
 
-    //Firebase
-    implementation("com.google.firebase:firebase-bom:30.3.1")
+    // Firebase
+    implementation(platform(libs.com.google.firebase.firebase.bom))
     implementation(libs.com.google.firebase.firebase.crashlytics.ktx)
     implementation(libs.com.google.firebase.firebase.analytics.ktx)
     implementation(libs.com.google.firebase.firebase.perf.ktx)
 
-    //Coil
+    // Coil
     implementation(libs.io.coil.kt.coil.compose)
 
-    //Room
+    // Room
     implementation(libs.androidx.room.room.ktx)
     implementation(libs.androidx.room.room.paging)
-    kapt(libs.androidx.room.room.compiler)
+    ksp(libs.androidx.room.room.compiler)
 
-    //Camera
+    // Camera TODO remove
     implementation(libs.androidx.camera.camera.core)
     implementation(libs.androidx.camera.camera.camera2)
     implementation(libs.androidx.camera.camera.lifecycle)
@@ -147,17 +141,17 @@ dependencies {
     implementation(libs.androidx.camera.camera.view)
     implementation(libs.androidx.camera.camera.extensions)
 
-    //MLKit
-//    implementation(libs.com.google.mlkit.text.recognition)
+    // MLKit
+    //    implementation(libs.com.google.mlkit.text.recognition)
 
-    //Timber
+    // Timber
     implementation(libs.com.jakewharton.timber)
 
-    //Compose-Destinations
+    // Compose-Destinations
     implementation(libs.io.github.raamcosta.compose.destinations.animations.core)
     ksp(libs.io.github.raamcosta.compose.destinations.ksp)
 
-    //Accompanist
+    // Accompanist
     implementation(libs.com.google.accompanist.accompanist.systemuicontroller)
     implementation(libs.com.google.accompanist.accompanist.placeholder)
     implementation(libs.com.google.accompanist.accompanist.flowlayout)
@@ -166,11 +160,19 @@ dependencies {
     implementation(libs.com.google.accompanist.accompanist.pager.indicators)
     implementation(libs.com.google.accompanist.accompanist.permissions)
 
-    //Chucker
+    // Chucker
     debugImplementation(libs.com.github.chuckerteam.chucker.library)
     releaseImplementation(libs.com.github.chuckerteam.chucker.library.no.op)
 
     //Palette
     implementation(libs.androidx.palette.palette.ktx)
+
+    // Unit test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.espresso.core)
+    androidTestImplementation(libs.androidx.compose.ui.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.ui.test.manifest)
 
 }
