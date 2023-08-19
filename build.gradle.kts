146e0d1e6a6b9d8ab92e13343a3beb0d68e29bd4
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-20 00:40:07 +0530

    removed class path section

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,4 @@
-buildscript {
-//    ext {
-//        compose_version = '1.3.0-rc01'
-//    }
-    repositories {
-        mavenCentral()
-        google()
-    }
-//    dependencies {
-//        classpath libs.com.google.firebase.crashlytics
-//        classpath 'com.google.firebase:firebase-crashlytics-gradle:2.9.2'
-//        classpath "com.google.firebase:perf-plugin:1.4.2"
-//        classpath "com.google.gms:google-services:4.3.14"
-//        classpath "com.google.dagger:hilt-android-gradle-plugin:2.44"
-//    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.com.android.application).apply(false)
     alias(libs.plugins.com.android.library).apply(false)
