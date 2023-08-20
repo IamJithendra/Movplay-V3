56b9661010768b2a07fe2e7bc9e554befa89eb33
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-20 17:14:11 +0530

    changed favourite Type screen style

--- app/src/main/java/com/jvktech/moviebuff/ui/components/selectors/FavoriteTypeSelector.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/components/selectors/FavoriteTypeSelector.kt
@@ -1,22 +1,23 @@
 package com.jvktech.moviebuff.ui.components.selectors
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.jvktech.moviebuff.data.model.FavoriteType
 import com.jvktech.moviebuff.ui.theme.spacing
@@ -29,11 +30,17 @@ fun FavoriteTypeSelector(
 ) {
     Row(
         modifier = modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = MaterialTheme.shapes.small
+            )
+            .clip(shape = MaterialTheme.shapes.small),
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         FavoriteType.values().map { type ->
             FavouriteTypeButton(
+                modifier = Modifier.weight(1f),
                 type = type,
                 selected = type == selected,
                 onClick = { onSelected(type) }
@@ -50,54 +57,29 @@ fun FavouriteTypeButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
-    val textColor by animateColorAsState(
+    val backgroundColor by animateColorAsState(
         targetValue = if (selected) {
             MaterialTheme.colorScheme.primary
-        } else MaterialTheme.colorScheme.onBackground
+        } else Color.Transparent,
+        label = ""
     )
-    val lineLength = animateFloatAsState(
-        targetValue = if (selected) 2f else 0f,
-        animationSpec = tween(
-            durationMillis = 200
-        )
+    val textColor by animateColorAsState(
+        targetValue = if (selected) {
+            Color.White
+        } else MaterialTheme.colorScheme.primary,
+        label = ""
     )
-    val currentOption = MaterialTheme.colorScheme.primary
-    Text(
-        modifier = modifier
-            .padding(MaterialTheme.spacing.medium)
-            .drawBehind {
-                if (selected) {
-                    if (lineLength.value > 0f) {
-                        drawLine(
-                            color = if (selected) {
-                                currentOption
-                            } else {
-                                Color.Magenta
-                            },
-                            start = Offset(
-                                size.width / 2f - lineLength.value * 10.dp.toPx(),
-                                size.height
-                            ),
-                            end = Offset(
-                                size.width / 2f + lineLength.value * 10.dp.toPx(),
-                                size.height
-                            ),
-                            strokeWidth = 5.dp.toPx(),
-                            cap = StrokeCap.Round
-                        )
-                    }
-                }
-            }
-            .clickable(
-                indication = null,
-                interactionSource = remember {
-                    MutableInteractionSource()
-                }) {
-                onClick()
-            },
-        text = stringResource(type.getLabelResourceId()),
-        color = textColor,
-        style = MaterialTheme.typography.headlineMedium,
 
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .clickable { onClick() }
+            .padding(MaterialTheme.spacing.small),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(type.getLabelResourceId()),
+            color = textColor, fontWeight = FontWeight.Bold
         )
+    }
 }
