65c060e22425f23ec5980b184d18e69517200052
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-22 15:55:30 +0530

    fix nav bar background

--- app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
@@ -13,14 +13,17 @@ import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.jvktech.moviebuff.ui.screens.destinations.FavoriteScreenDestination
 import com.jvktech.moviebuff.ui.screens.destinations.MovieScreenDestination
 import com.jvktech.moviebuff.ui.screens.destinations.TvShowScreenDestination
@@ -59,7 +62,10 @@ fun BottomBar(
         exit = slideOutVertically { it }
     ) {
         NavigationBar(
-            modifier = modifier
+            modifier = modifier,
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = Color.Cyan,
+            tonalElevation = 4.dp
         ) {
             NavBarItem(
                 selected = selectedRoute == MovieScreenDestination.route,
