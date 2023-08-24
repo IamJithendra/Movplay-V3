1bbeaa7df07b3bc1a5804e25c8ee678dec576f4e
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-24 16:18:04 +0530

    change items on bottom navigation bar

--- app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
@@ -3,18 +3,18 @@ package com.jvktech.moviebuff.ui.components.others
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Movie
-import androidx.compose.material.icons.filled.SmartDisplay
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.Movie
-import androidx.compose.material.icons.outlined.SmartDisplay
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,11 +26,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.jvktech.moviebuff.R
 import com.jvktech.moviebuff.ui.screens.destinations.FavoriteScreenDestination
 import com.jvktech.moviebuff.ui.screens.destinations.MovieScreenDestination
 import com.jvktech.moviebuff.ui.screens.destinations.TvShowScreenDestination
@@ -75,10 +71,10 @@ fun BottomBar(
                 onClick = {
                     onItemClicked(MovieScreenDestination.route)
                 },
-                label = stringResource(R.string.movies_label),
-                selectedIcon = Icons.Filled.Movie,
-                unSelectedIcon = Icons.Outlined.Movie,
-                contentDescription = "Movie",
+                label = "Home",
+                selectedIcon = Icons.Rounded.Home,
+                unSelectedIcon = Icons.Outlined.Home,
+                contentDescription = "Home",
                 badgeCount = 7
 
             )
@@ -87,34 +83,24 @@ fun BottomBar(
                 onClick = {
                     onItemClicked(TvShowScreenDestination.route)
                 },
-                label = stringResource(R.string.tv_series_label),
-                selectedIcon = Icons.Filled.SmartDisplay,
-                unSelectedIcon = Icons.Outlined.SmartDisplay,
-                contentDescription = "Tv Show"
+                label = "Discover",
+                selectedIcon = Icons.Rounded.Search,
+                unSelectedIcon = Icons.Outlined.Search,
+                contentDescription = "Discover"
             )
             NavBarItem(
                 selected = selectedRoute == FavoriteScreenDestination.route,
                 onClick = {
                     onItemClicked(FavoriteScreenDestination.route)
                 },
-                label = stringResource(R.string.favourites_label),
-                selectedIcon = Icons.Filled.Favorite,
-                unSelectedIcon = Icons.Outlined.FavoriteBorder,
-                contentDescription = "Favorites",
+                label = "Watchlist",
+                selectedIcon = Icons.Filled.Bookmark,
+                unSelectedIcon = Icons.Outlined.BookmarkBorder,
+                contentDescription = "Watchlist",
                 hasNews = true
 
             )
 
-//            NavBarItem(
-//                selected = selectedRoute == SearchScreenDestination.route,
-//                onClick = {
-//                    onItemClicked(SearchScreenDestination.route)
-//                },
-//                label = stringResource(R.string.search_label),
-//                selectedIcon = Icons.Filled.ZoomIn,
-//                unSelectedIcon = Icons.Outlined.Search,
-//                contentDescription = "Search"
-//            )
         }
     }
 }
