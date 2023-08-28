d7eb74693016024faa4b8f6f9b2f7b74c121b45d
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-28 17:15:20 +0530

    replaced input text field with AssistChip

--- app/src/main/java/com/jvktech/moviebuff/ui/screens/search/components/DiscoverInputField.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/screens/search/components/DiscoverInputField.kt
@@ -1,14 +1,16 @@
 package com.jvktech.moviebuff.ui.screens.search.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jvktech.moviebuff.ui.screens.destinations.SearchScreenDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -18,23 +20,25 @@ fun DiscoverScreenTextField(
     navigator: DestinationsNavigator,
 ) {
 
-    androidx.compose.material3.OutlinedTextField(
-        enabled = false,
-        value = "",
-        onValueChange = { /* Handle text change */ },
-        label = { androidx.compose.material3.Text(text = "Search Movie, show or Actor") },
-        leadingIcon = {
-            androidx.compose.material3.Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null
+    AssistChip(
+        modifier = Modifier
+            .fillMaxWidth(),
+        onClick = { navigator.navigate(SearchScreenDestination.route) },
+        label = {
+            androidx.compose.material3.Text(
+                modifier = Modifier.padding(start=5.dp),
+                text = "Search Movie, show or Actor",
+                color = MaterialTheme.colorScheme.primary
             )
         },
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = {
-                navigator.navigate(SearchScreenDestination.route)
-            }),
-        colors = TextFieldDefaults.outlinedTextFieldColors()
+        leadingIcon = {
+            androidx.compose.material3.Icon(
+                Icons.Filled.Search,
+                contentDescription = "Localized description",
+                Modifier.size(AssistChipDefaults.IconSize)
+            )
+        }
     )
+
 }
 
