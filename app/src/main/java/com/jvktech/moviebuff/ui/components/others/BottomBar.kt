8c971cc717120ccd52466125ddcf0559d0b694bb
Jithendra V Kumar <jithendra1998@gmail.com>
2023-08-28 15:50:24 +0530

    fixed Discover on bottom navigation not active on select

--- app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/components/others/BottomBar.kt
@@ -43,7 +43,7 @@ fun BottomBar(
     val bottomBarRoutes = remember {
         mutableSetOf(
             HomeScreenDestination.route,
-            TvShowScreenDestination.route,
+            DiscoverScreenDestination.route,
             WatchlistScreenDestination.route,
         )
     }
