f9dd81f85afbaadf4c040a29654ec7c242f0c43c
Jithendra V Kumar <jithendra1998@gmail.com>
2023-10-07 23:33:27 +0530

    added privacy policy and Tnc

--- app/src/main/java/com/jvktech/moviebuff/ui/components/others/About.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/components/others/About.kt
@@ -1,34 +1,18 @@
 package com.jvktech.moviebuff.ui.components.others
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MonetizationOn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.jvktech.moviebuff.R
+import androidx.constraintlayout.compose.ConstraintLayout
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,36 +30,61 @@ fun AboutBottomSheet(
         containerColor = MaterialTheme.colorScheme.background
     ) {
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            // App Icon
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground), // Replace with your app icon resource
-                contentDescription = "App Icon",
-                modifier = Modifier.size(100.dp)
-            )
-
-            // Title
-            Text(
-                text = "Thank you for using Movie buff!",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(8.dp)
-            )
-
-            // Subtitle
-            Text(
-                text = "You can donate to help us in improving the app, or just to show an appreciation to our work",
-                style = MaterialTheme.typography.bodySmall,
-                modifier = Modifier.padding(8.dp)
-            )
+        Scaffold(
+            content = { innerPadding ->
+                ConstraintLayout(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                ) {
+                    val (buttonsRow) = createRefs()
 
 
+                    // Rest of your layout remains the same.
+                    // ...
 
+                    // Align the buttons row to the bottom center
+                    val buttonsAlignment = Modifier.constrainAs(buttonsRow) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
 
+                    // Buttons for privacy policy and terms of use
+                    Row(
+                        modifier = buttonsAlignment
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp), // Adjust bottom padding as needed
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        TextButton(
+                            onClick = {
+                                // Handle click event for Privacy Policy button here
+                            },
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                        ) {
+                            Text(text = "Privacy policy")
+                        }
 
-        }
+                        Text(
+                            text = "•",
+                            modifier = Modifier
+                                .padding(vertical = 16.dp)
+                        )
+
+                        TextButton(
+                            onClick = {
+                                // Handle click event for Terms of Use button here
+                            },
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                        ) {
+                            Text(text = "Terms of use")
+                        }
+                    }
+                }
+            }
+        )
+
     }
 }
 
