f1702374f7fd4b6f5b9863f709c6b46c0b0d046e
Jithendra V Kumar <jithendra1998@gmail.com>
2023-11-05 00:32:45 +0530

    working on new details screen

--- app/src/main/java/com/jvktech/moviebuff/ui/screens/details/movie/MovieDetailsScreen2.kt
+++ app/src/main/java/com/jvktech/moviebuff/ui/screens/details/movie/MovieDetailsScreen2.kt
@@ -1,6 +1,8 @@
 package com.jvktech.moviebuff.ui.screens.details.movie
 
+import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,21 +23,152 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.jvktech.moviebuff.R
+import com.jvktech.moviebuff.data.model.ExternalId
+import com.jvktech.moviebuff.data.model.MediaType
+import com.jvktech.moviebuff.data.model.RelationType
+import com.jvktech.moviebuff.data.model.ShareDetails
+import com.jvktech.moviebuff.data.model.Video
+import com.jvktech.moviebuff.data.model.movie.MovieDetails
+import com.jvktech.moviebuff.ui.screens.destinations.MovieDetailsScreenDestination
+import com.jvktech.moviebuff.ui.screens.destinations.PersonDetailsScreenDestination
+import com.jvktech.moviebuff.ui.screens.destinations.RelatedMoviesScreenDestination
+import com.jvktech.moviebuff.ui.screens.destinations.ReviewsScreenDestination
+import com.jvktech.moviebuff.utils.openExternalId
+import com.jvktech.moviebuff.utils.openVideo
+import com.jvktech.moviebuff.utils.shareImdb
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+
+@Destination(
+    navArgsDelegate = MovieDetailsScreenArgs::class,
+    style = MovieDetailsScreenTransitions::class
+)
+@Composable
+fun AnimatedVisibilityScope.MovieDetailsScreen2(
+    viewModel: MovieDetailsScreenViewModel = hiltViewModel(),
+    navigator: DestinationsNavigator
+) {
+    val context = LocalContext.current
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val onBackClicked: () -> Unit = { navigator.navigateUp() }
+    val onFavouriteClicked: (details: MovieDetails) -> Unit = { details ->
+        if (uiState.additionalMovieDetailsInfo.isFavorite) {
+            viewModel.onUnlikeClick(details)
+        } else {
+            viewModel.onLikeClick(details)
+        }
+    }
+
+    val onExternalIdClicked = { id: ExternalId ->
+        openExternalId(
+            context = context,
+            externalId = id
+        )
+    }
+    val onShareClicked = { details: ShareDetails ->
+        shareImdb(
+            context = context,
+            details = details
+        )
+    }
+    val onVideoClicked = { video: Video ->
+        openVideo(
+            context = context,
+            video = video
+        )
+    }
+    val onMemberClicked = { personId: Int ->
+        val destination = PersonDetailsScreenDestination(
+            personId = personId,
+            startRoute = uiState.startRoute
+        )
+
+        navigator.navigate(destination)
+    }
+    val onMovieClicked = { movieId: Int ->
+        val destination = MovieDetailsScreenDestination(
+            movieId = movieId,
+            startRoute = uiState.startRoute
+        )
+
+        navigator.navigate(destination)
+    }
+
+    val onReviewsClicked: () -> Unit = {
+        val movieId = uiState.movieDetails?.id
+
+        if (movieId != null) {
+            val destination = ReviewsScreenDestination(
+                startRoute = uiState.startRoute,
+                mediaId = movieId,
+                type = MediaType.Movie
+            )
+
+            navigator.navigate(destination)
+        }
+    }
+
+    val onSimilarMoreClicked = {
+        val movieId = uiState.movieDetails?.id
+
+        if (movieId != null) {
+            val destination = RelatedMoviesScreenDestination(
+                movieId = movieId,
+                type = RelationType.Similar,
+                startRoute = uiState.startRoute
+            )
+
+            navigator.navigate(destination)
+        }
+    }
+
+    val onRecommendationsMoreClicked = {
+        val movieId = uiState.movieDetails?.id
+
+        if (movieId != null) {
+            val destination = RelatedMoviesScreenDestination(
+                movieId = movieId,
+                type = RelationType.Recommended,
+                startRoute = uiState.startRoute
+            )
+
+            navigator.navigate(destination)
+        }
+    }
+    MovieDetailsScreenContent2(
+//        uiState = uiState,
+//        onBackClicked = onBackClicked,
+//        onExternalIdClicked = onExternalIdClicked,
+//        onShareClicked = onShareClicked,
+//        onVideoClicked = onVideoClicked,
+//        onFavouriteClicked = onFavouriteClicked,
+//        onMemberClicked = onMemberClicked,
+//        onMovieClicked = onMovieClicked,
+//        onSimilarMoreClicked = onSimilarMoreClicked,
+//        onRecommendationsMoreClicked = onRecommendationsMoreClicked,
+//        onReviewsClicked = onReviewsClicked
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MovieDetailsScreen2(
+fun MovieDetailsScreenContent2(
 
 ) {
 
@@ -44,7 +177,7 @@ fun MovieDetailsScreen2(
             TopAppBar(
                 title = {
                     Text(
-                        text = "Icc World cup",
+                        text = "Leo",
                     )
                 },
                 navigationIcon = {
@@ -80,11 +213,12 @@ fun MovieDetailsScreen2(
                 contentScale = ContentScale.Crop,
             )
 
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(start = 8.dp, bottom = 8.dp),
-                verticalAlignment = Alignment.Bottom
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 // Poster Image
                 Box(
@@ -101,47 +235,48 @@ fun MovieDetailsScreen2(
                 }
 
                 // Title Text
-                Column(
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                        .align(Alignment.Bottom)
-                ) {
-                    Text(
-                        text = "Leo",
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    )
+                Text(
+                    text = "Leo",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
 
-                    // Certification, Release Date, and Runtime in a Row scope
-                    Row {
-                        Text(
-                            text = "U/A",
-                            fontSize = 14.sp, fontWeight = FontWeight.Bold,
-                            color = Color.White, modifier = Modifier.padding(end = 4.dp)
-                        )
-                        Text(
-                            text = "Apr 2021",
-                            fontSize = 14.sp, fontWeight = FontWeight.Bold,
-                            color = Color.White, modifier = Modifier.padding(horizontal = 4.dp)
-                        )
-                        Text(
-                            text = "\u00B7", // Unicode for middle dot (·)
-                            fontSize = 14.sp,
-                            color = Color.White,
-                            modifier = Modifier.padding(horizontal = 4.dp)
-                        )
 
-                        Text(
-                            text = "124 Mins",
-                            fontSize = 14.sp, fontWeight = FontWeight.Bold,
-                            color = Color.White, modifier = Modifier.padding(horizontal = 4.dp)
-                        )
-                    }
-                }
             }
         }
 
+//// Certification, Release Date, and Runtime in a Row scope
+//        Row {
+//            Text(
+//                text = "U/A",
+//                fontSize = 14.sp,
+//                fontWeight = FontWeight.Bold,
+//                color = Color.White,
+//                modifier = Modifier.padding(end = 4.dp)
+//            )
+//            Text(
+//                text = "16 Apr 2021",
+//                fontSize = 14.sp,
+//                fontWeight = FontWeight.Bold,
+//                color = Color.White,
+//                modifier = Modifier.padding(horizontal = 4.dp)
+//            )
+//            Text(
+//                text = "\u00B7", // Unicode for middle dot (·)
+//                fontSize = 14.sp,
+//                color = Color.White,
+//                modifier = Modifier.padding(horizontal = 4.dp)
+//            )
+//
+//            Text(
+//                text = "124 Mins",
+//                fontSize = 14.sp,
+//                fontWeight = FontWeight.Bold,
+//                color = Color.White,
+//                modifier = Modifier.padding(horizontal = 4.dp)
+//            )
+//        }
 
     }
 }
@@ -149,5 +284,5 @@ fun MovieDetailsScreen2(
 @Composable
 @Preview
 fun MovieDetailsScreen2Preview() {
-    MovieDetailsScreen2()
+    MovieDetailsScreenContent2()
 }
