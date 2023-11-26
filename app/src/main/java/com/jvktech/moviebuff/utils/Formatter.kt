a6f51438673d187ec8cd5d5c1ce661efc122d266
Jithendra V Kumar <jithendra1998@gmail.com>
2023-11-27 05:13:46 +0530

    added function to convert time to readable time

--- app/src/main/java/com/jvktech/moviebuff/utils/Formatter.kt
+++ app/src/main/java/com/jvktech/moviebuff/utils/Formatter.kt
@@ -1,13 +1,14 @@
 package com.jvktech.moviebuff.utils
 
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.time.Duration.Companion.minutes
 
 fun Int.formattedRuntime(): String? {
     return minutes.toComponents { hours, minutes, _, _ ->
-        val hoursString = if (hours > 0) "${hours}h" else null
-        val minutesString = if (minutes > 0) "${minutes}m" else null
+        val hoursString = if (hours > 0) "${hours}hr" else null
+        val minutesString = if (minutes > 0) "${minutes}min" else null
 
         listOfNotNull(hoursString, minutesString).run {
             if (isEmpty()) null else joinToString(separator = " ")
@@ -23,4 +24,10 @@ fun Long.formattedMoney(): String {
     return NumberFormat.getCurrencyInstance(Locale.US).apply {
         maximumFractionDigits = 0
     }.format(this).replace(",", " ")
+}
+
+
+fun convertDate(inputDate: Date): String {
+    val outputFormat = SimpleDateFormat("EEE, dd MMM yyyy", Locale.ENGLISH)
+    return outputFormat.format(inputDate)
 }
